1a0cc110e0e7074f3a4b2fb7cbcaf302873bf04e
ScottShadow <rudaseswascottmc@gmail.com>
2024-08-28 07:59:46 +0200

    bug fixes

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,18 +1,23 @@
 export default function createPushNotificationsJobs(jobs, queue) {
-    if (Array.isArray(jobs)) {
-        jobs.forEach((data) => {
-            const Push3 = queue.create('push_notification_code_3', data).save((err) => {
-                if (!err) console.log(`Notification job created: ${Push3.id}`);
-            });
-            Push3.on('complete', () => {
-                console.log(`Notification job ${Push3.id} completed`);
-            }).on('progress', (progress) => {
-                console.log(`Notification job ${Push3.id} ${progress}% complete`);
-            }).on('failed', (err) => {
-                console.log(`Notification job $${Push3.id} failed: ${err}`);
-            });
-        });
-    } else {
-        throw new Error('Jobs is not an array');
-    }
+  if (Array.isArray(jobs)) {
+    jobs.forEach((data) => {
+      const Push3 = queue.create('push_notification_code_3', data).save((err) => {
+        if (!err && Push3) {
+          console.log(`Notification job created: ${Push3.id}`);
+          Push3.on('complete', () => {
+            console.log(`Notification job ${Push3.id} completed`);
+          }).on('progress', (progress) => {
+            console.log(`Notification job ${Push3.id} ${progress}% complete`);
+          }).on('failed', (err) => {
+            console.log(`Notification job $${Push3.id} failed: ${err}`);
+          });
+        } else {
+          console.error('Failed to create job');
+        }
+      });
+
+    });
+  } else {
+    throw new Error('Jobs is not an array');
+  }
 }
--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,39 +1,40 @@
 import kue from 'kue';
-import {describe, it} from 'mocha';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
 import createPushNotificationsJobs from './8-job';
 
 const queue = kue.createQueue();
 
 const list = [
-    {
-        phoneNumber: '4153518780',
-        message: 'This is the code 1234 to verify your account',
-    },
-    {
-        phoneNumber: '4153518781',
-        message: 'This is the code 12345 to verify your account',
-    },
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account',
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 12345 to verify your account',
+  },
 ];
 
 describe('does something cool', () => {
-    before(() => {
-        queue.testMode.enter();
-    });
+  before(() => {
+    queue.testMode.enter();
+  });
 
-    afterEach(() => {
-        queue.testMode.clear();
-    });
+  afterEach(() => {
+    queue.testMode.clear();
+  });
 
-    after(() => {
-        queue.testMode.exit();
-    });
+  after(() => {
+    queue.testMode.exit();
+  });
 
-    it('core test part', () => {
-        createPushNotificationsJobs(list, queue);
-        expect(queue.testMode.jobs.length).to.equal(2);
-        expect(queue.testMode.jobs[0].data).to.eql({
-            phoneNumber: '4153518780',
-            message: 'This is the code 1234 to verify your account',
-        });
-    })
+  it('core test part', () => {
+    createPushNotificationsJobs(list, queue);
+    expect(queue.testMode.jobs.length).to.equal(2);
+    expect(queue.testMode.jobs[0].data).to.eql({
+      phoneNumber: '4153518780',
+      message: 'This is the code 1234 to verify your account',
+    });
+  })
 });
